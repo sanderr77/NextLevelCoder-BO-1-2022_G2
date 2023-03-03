aa6608d34e4dccbfcd853375a15e3a6426b984f5
sanderr77 <126743610+sanderr77@users.noreply.github.com>
2023-03-03 16:23:00 -0400

    clase 1: introduccion

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,16 @@
+#clase dinosaur, metodo constructor, update, draw pass, 
+# investigar que son los Sprites
+#investigar herencia en programacion
+#poner una imagen a sus cuentas de team :3
+import pygame
+from dino_runner.utils.constants import DINO_IMG
+
+class Dinosaur:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
