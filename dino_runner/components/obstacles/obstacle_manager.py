131cbbebfad1304dd72193382d85bcdf1011db1d
sanderr77 <126743610+sanderr77@users.noreply.github.com>
2023-03-07 18:22:45 -0400

    Class 4
    (antes de corregir)

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,28 +1,46 @@
+from dino_runner.components.obstacles.bird import Bird
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
+
 import pygame
 import random
 
-CACTUS_LIST = [SMALL_CACTUS, LARGE_CACTUS]
-
 class ObstacleManager:
+
     def __init__(self):
         self.obstacles = []
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(random.choice(CACTUS_LIST)))
+            if random.choice([True, False]):
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            else:
+                self.obstacles.append(Cactus(LARGE_CACTUS))
+            
+            if random.choice([True, False]):
+                self.obstacles.append(Bird(BIRD))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
-                break
+                break 
 
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+    
+    def check_collisions (self):
+        for i, obstacle in enumerate(self.obstacles):
+            for other_obstacle in self.obstacles[i+1:]:
+                if obstacle.rect.colliderect(other_obstacle.rect):
+                    if obstacle.rect.x <= other_obstacle.rect.x:
+                        self.obstacles.remove(obstacle)
+                    else:
+                        self.obstacles.remove(other_obstacle)
+                    break
+
     
 
 #agregar el bird 
